d6b45838c88063f3d6a429e6f6ce3d0a7bb738d8
iyiolacak <iyiolacak0@gmail.com>
2026-08-08 12:09:31 +0300

    Remove generated source declarations

--- packages/mood-wheel/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { MoodWheel, MOOD_WHEEL_ASSETS } from "./MoodWheel";
-export type { MoodWheelAssets, MoodWheelChange, MoodWheelChangeSource, MoodWheelMessages, MoodWheelOption, MoodWheelProps, } from "./MoodWheel";
-export { MAX_MOOD_WHEEL_OPTIONS, MIN_MOOD_WHEEL_OPTIONS, clampWheelIndex, projectedWheelIndex, wheelOffsetFromDrag, wheelOptionAngle, wheelRotation, wheelSlotWidth, } from "./model";
-//# sourceMappingURL=index.d.ts.map
